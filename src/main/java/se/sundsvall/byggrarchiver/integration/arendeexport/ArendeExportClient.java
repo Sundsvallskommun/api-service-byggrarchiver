2b30ffd04a531147688eb992dfbe22329c6a85b1
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2023-03-13 19:21:46 +0100

    UF-4601 - Skip external generated client
    Moves the external generated client for "ärendeexport" into the service.
    
    Also, cleans up/removes some stuff such as the Maven wrapper and some Quarkus leftovers

--- src/main/java/se/sundsvall/byggrarchiver/integration/arendeexport/ArendeExportClient.java
+++ src/main/java/se/sundsvall/byggrarchiver/integration/arendeexport/ArendeExportClient.java
@@ -1,16 +1,16 @@
 package se.sundsvall.byggrarchiver.integration.arendeexport;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import se.sundsvall.byggrarchiver.integration.arendeexport.configuration.ArendeExportConfiguration;
+
 import arendeexport.GetDocument;
 import arendeexport.GetDocumentResponse;
 import arendeexport.GetUpdatedArenden;
 import arendeexport.GetUpdatedArendenResponse;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import se.sundsvall.byggrarchiver.integration.arendeexport.configuration.ArendeExportConfiguration;
 
 @FeignClient(name = "arendeexport", url = "${integration.arendeexport.url}", configuration = ArendeExportConfiguration.class)
-@CircuitBreaker(name = "arendeexport")
 public interface ArendeExportClient {
 
     String TEXT_XML_UTF8 = "text/xml;charset=UTF-8";
--- src/main/java/se/sundsvall/byggrarchiver/integration/sundsvall/archive/ArchiveClient.java
+++ src/main/java/se/sundsvall/byggrarchiver/integration/sundsvall/archive/ArchiveClient.java
@@ -1,16 +1,16 @@
 package se.sundsvall.byggrarchiver.integration.sundsvall.archive;
 
-import generated.se.sundsvall.archive.ArchiveResponse;
-import generated.se.sundsvall.archive.ByggRArchiveRequest;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import se.sundsvall.byggrarchiver.integration.sundsvall.archive.configuration.ArchiveConfiguration;
 
+import generated.se.sundsvall.archive.ArchiveResponse;
+import generated.se.sundsvall.archive.ByggRArchiveRequest;
+
 @FeignClient(name = "archive", url = "${integration.archive.url}", configuration = ArchiveConfiguration.class)
-@CircuitBreaker(name = "archive")
 public interface ArchiveClient {
 
     @PostMapping(path = "/archive/byggr", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)

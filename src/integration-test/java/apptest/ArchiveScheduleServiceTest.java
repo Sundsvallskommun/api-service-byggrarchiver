1f2360b6f2936b1c6bfb33b5962c9ce9e26a6d14
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-10-04 16:11:44 +0200

    UF-5984:

--- src/integration-test/java/apptest/ArchiveScheduleServiceTest.java
+++ src/integration-test/java/apptest/ArchiveScheduleServiceTest.java
@@ -1,33 +1,33 @@
 package apptest;
 
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.TestPropertySource;
+
 import se.sundsvall.byggrarchiver.Application;
 import se.sundsvall.byggrarchiver.service.ArchiverScheduleService;
 import se.sundsvall.dept44.test.AbstractAppTest;
 import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
 
-import java.time.Duration;
-
-import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
-
-@TestPropertySource(properties = {"cron.expression=* * * ? * *"})
+@TestPropertySource(properties = { "cron.expression=* * * ? * *" })
 @WireMockAppTestSuite(
-        files = "classpath:/IntegrationTest/",
-        classes = Application.class
-)
+	files = "classpath:/IntegrationTest/",
+	classes = Application.class)
 class ArchiveScheduleServiceTest extends AbstractAppTest {
 
-    @SpyBean
-    private ArchiverScheduleService archiverScheduleService;
+	@SpyBean
+	private ArchiverScheduleService archiverScheduleService;
 
-    @Test
-    void archive() {
-        await()
-                .atMost(Duration.ofSeconds(10))
-                .untilAsserted(() -> verify(archiverScheduleService, atLeast(9)).archive());
-    }
+	@Test
+	void archive() {
+		await()
+			.atMost(Duration.ofSeconds(30))
+			.untilAsserted(() -> verify(archiverScheduleService, atLeast(5)).archive());
+	}
 }

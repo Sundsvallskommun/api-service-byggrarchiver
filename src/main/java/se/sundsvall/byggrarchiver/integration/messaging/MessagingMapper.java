9857d7af4db85e7b8ce9b3fed1e133d618c534cc
cheezi0747 <linus.sjolinder@b3.se>
2024-06-18 19:50:26 +0200

    UF-8861: Updated messaging-integration

---
+++ src/main/java/se/sundsvall/byggrarchiver/integration/messaging/MessagingMapper.java
@@ -0,0 +1,30 @@
+package se.sundsvall.byggrarchiver.integration.messaging;
+
+import java.util.Map;
+
+import generated.se.sundsvall.messaging.EmailRequest;
+import generated.se.sundsvall.messaging.EmailSender;
+
+public final class MessagingMapper {
+
+	private static final String BYGGR_ARCHIVER_SENDER = "ByggrArchiver";
+
+
+	private MessagingMapper() {
+		// Prevent instantiation
+	}
+
+	static EmailRequest toEmailRequest(final String sender, final String htmlMessage, final String recipient, final String subject) {
+		return new EmailRequest().sender(toEmailSender(sender))
+			.emailAddress(recipient)
+			.subject(subject)
+			.htmlMessage(htmlMessage);
+	}
+
+	static EmailSender toEmailSender(final String sender) {
+		return new EmailSender()
+			.name(BYGGR_ARCHIVER_SENDER)
+			.address(sender);
+	}
+
+}

c024acd5d8fe1c0424b70d35f6b63a5e628590c3
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2024-12-02 10:38:56 +0100

    UF-11202 - Missing municipality id:s
    Adds validation on archive scheduler properties.

--- src/main/java/se/sundsvall/byggrarchiver/service/scheduler/ArchiverScheduler.java
+++ src/main/java/se/sundsvall/byggrarchiver/service/scheduler/ArchiverScheduler.java
@@ -1,6 +1,5 @@
 package se.sundsvall.byggrarchiver.service.scheduler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -17,27 +16,33 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 @Service
 public class ArchiverScheduler {
 
-	private static final Logger log = LoggerFactory.getLogger(ArchiverScheduler.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ArchiverScheduler.class);
 
 	private final ByggrArchiverService byggrArchiverService;
-
 	private final SchedulerProperties schedulerProperties;
 
 	public ArchiverScheduler(final ByggrArchiverService byggrArchiverService, final SchedulerProperties schedulerProperties) {
 		this.byggrArchiverService = byggrArchiverService;
 		this.schedulerProperties = schedulerProperties;
+
+		if ("-".equals(schedulerProperties.cron().expression())) {
+			LOG.info("ArchiverScheduler is DISABLED");
+		} else {
+			LOG.info("ArchiverScheduler is ENABLED, with cron expression {}", schedulerProperties.cron().expression());
+		}
 	}
 
 	@Scheduled(cron = "${scheduler.cron.expression}")
 	@SchedulerLock(name = "archive", lockAtMostFor = "${scheduler.shedlock-lock-at-most-for}")
 	public void archive() {
-		log.info("Running archiving on schedule. Timestamp: {}", LocalDateTime.now(ZoneId.systemDefault()));
+		var now = LocalDateTime.now(ZoneId.systemDefault());
+
+		LOG.info("Running archiving on schedule. Timestamp: {}", now);
 
 		// Run batch from one week back in time to yesterday
-		final LocalDate oneWeekBack = LocalDate.now(ZoneId.systemDefault()).minusDays(7);
-		final LocalDate yesterday = LocalDate.now(ZoneId.systemDefault()).minusDays(1);
+		var oneWeekBack = now.toLocalDate().minusDays(7);
+		var yesterday = now.toLocalDate().minusDays(1);
 
 		schedulerProperties.municipalityIds().forEach(municipalityId -> byggrArchiverService.runBatch(oneWeekBack, yesterday, BatchTrigger.SCHEDULED, municipalityId));
 	}
-
 }

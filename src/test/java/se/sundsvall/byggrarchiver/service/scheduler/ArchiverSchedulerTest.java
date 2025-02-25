6306c659984d439e8d185b5ae7eee066a10453cb
Martin Hansson <martinhansson80@gmail.com>
2025-02-25 11:28:09 +0100

    uf-11195: Rebased and fixed tests.

--- src/test/java/se/sundsvall/byggrarchiver/service/scheduler/ArchiverSchedulerTest.java
+++ src/test/java/se/sundsvall/byggrarchiver/service/scheduler/ArchiverSchedulerTest.java
@@ -1,8 +1,5 @@
 package se.sundsvall.byggrarchiver.service.scheduler;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.platform.commons.util.AnnotationUtils.findAnnotation;
-import static org.junit.platform.commons.util.ReflectionUtils.findMethod;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -10,14 +7,12 @@ import static se.sundsvall.byggrarchiver.testutils.TestUtil.randomLong;
 
 import java.time.LocalDate;
 import java.util.List;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.scheduling.annotation.Scheduled;
 import se.sundsvall.byggrarchiver.api.model.BatchHistoryResponse;
 import se.sundsvall.byggrarchiver.api.model.enums.BatchTrigger;
 import se.sundsvall.byggrarchiver.service.ByggrArchiverService;
@@ -50,24 +45,5 @@ class ArchiverSchedulerTest {
 
 		verify(mockByggrArchiverService).runBatch(originalStart, end, batchTrigger, municipalityId);
 		verifyNoMoreInteractions(mockByggrArchiverService);
-	}
-
-	@Test
-	void testScheduledAnnotationContainsCorrectCronValue() {
-		var scheduledAnnotation = findMethod(ArchiverScheduler.class, "archive")
-			.flatMap(archive -> findAnnotation(archive, Scheduled.class))
-			.orElseThrow(() -> new IllegalStateException("Unable to find the 'archive' method on the " + ArchiverScheduler.class.getName() + " class"));
-
-		assertThat(scheduledAnnotation.cron()).isEqualTo("${scheduler.cron.expression}");
-	}
-
-	@Test
-	void testSchedulerLockAnnotationContainsCorrectNameAndLockAtMostForValue() {
-		var schedulerLockAnnotation = findMethod(ArchiverScheduler.class, "archive")
-			.flatMap(archive -> findAnnotation(archive, SchedulerLock.class))
-			.orElseThrow(() -> new IllegalStateException("Unable to find the 'archive' method on the " + ArchiverScheduler.class.getName() + " class"));
-
-		assertThat(schedulerLockAnnotation.name()).isEqualTo("archive");
-		assertThat(schedulerLockAnnotation.lockAtMostFor()).isEqualTo("${scheduler.shedlock-lock-at-most-for}");
 	}
 }
